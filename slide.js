7be97ae928c8825b5470ecaa7732bdc40ca28623
bitueboss <69850346+bitueboss@users.noreply.github.com>
2021-07-18 15:07:11 +0600

    added js and nav

---
+++ slide.js
@@ -0,0 +1,63 @@
+
+var container = document.getElementById('menu-bb')
+var button = document.getElementById('scrol')
+
+
+function toggle(newColor) {
+
+
+    var display = document.getElementById("menu-bb");
+    display.style.display = newColor
+    display.style.width = "80%"
+    display.style.margin = "0 auto"
+    var button = document.getElementById('scrol')
+    button.style.display = "none"
+    var logo = document.getElementById('logo')
+    logo.style.display = "none"
+}
+
+// button.addEventListener("click",slideToggle({
+
+//     var display= document.getElementById("menu-bb");
+//     var display= document.getElementById("menu-bb");
+//     display.style.display=newColor
+//     display.style.width="80%"
+//     display.style.margin="0 auto"
+//     var button    = document.getElementById('scrol')
+//     button.style.display="none"
+//     var logo    = document.getElementById('logo')
+//     logo.style.display="none"
+// }))
+
+
+
+// button.addEventListener("click",function(){
+
+// var slideDown = (target, duration = 500) => {
+//     if (window.getComputedStyle("menu-bb").display === 'block') {
+
+//     } 
+// }
+// })
+
+// function slideToggle(container, 1000, function(){
+//     let display= document.getElementById("menu-bb");
+//     display.style.display=newColor
+//     display.style.width="80%"
+//     display.style.margin="0 auto"
+//      let button    = document.getElementById('scrol')
+//     button.style.display="none"
+//     let logo    = document.getElementById('logo')
+//     logo.style.display="none"
+//       });
+
+// button.addEventListener("click", (slideToggle)=>{
+
+// })
+
+
+
+
+
+
+
